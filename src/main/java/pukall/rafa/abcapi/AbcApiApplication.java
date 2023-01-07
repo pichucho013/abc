5a0e449a10042ea880f4eea548be83a7d56e01ea
pichucho013 <61350129+pichucho013@users.noreply.github.com>
2023-01-07 00:00:58 -0300

    ABC API

---
+++ src/main/java/pukall/rafa/abcapi/AbcApiApplication.java
@@ -0,0 +1,13 @@
+package pukall.rafa.abcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pukall/rafa/abcapi/enums/ErrorCodigo.java
@@ -0,0 +1,8 @@
+package pukall.rafa.abcapi.enums;
+
+public enum ErrorCodigo {
+    g267,
+    g268,
+    g100
+
+}
